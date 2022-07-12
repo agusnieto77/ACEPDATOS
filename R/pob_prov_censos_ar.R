878e54eff2ae0c844fa7b1ce53fafc2c6a15b2c5
agusnieto77 <agustin.nieto77@gmail.com>
2022-07-12 18:25:19 -0300

    Actualización

---
+++ R/pob_prov_censos_ar.R
@@ -0,0 +1,10 @@
+#' @title Datos censales sobre población
+#' @description Datos sobre población. Se trata de una serie de datos sobre población discriminados por provincia, relevados por el INDEC.
+#' @format se trata de una tabla con 24 observaciones y 3 variables:
+#' \describe{
+#'   \item{prov}{dbl provincia}
+#'   \item{pob}{chr población}
+#'   \item{fecha}{chr año}
+#' }
+#' @source \url{https://www.indec.gob.ar/}
+"pob_prov_censos_ar"
